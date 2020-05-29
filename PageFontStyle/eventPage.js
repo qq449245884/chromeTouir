65d73ef3d19233c81142f8a0f5161dd68d096c82
qq449245884 <449245884@qq.com>
2020-05-29 22:37:50 +0800

    dd

--- PageFontStyle/eventPage.js
+++ PageFontStyle/eventPage.js
@@ -1,6 +1,11 @@
-chrome.tabs.query({
-  active: true,
-  currentWindow: true
-}, function(tabs) {
-  chrome.pageAction.show(tabs[0].id)
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  if (request.todo === "showPageAction"){
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, function(tabs) {
+        chrome.pageAction.show(tabs[0].id)
+      })
+  }
 })
+
