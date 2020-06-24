bea95ebac5c93f3cac6e091b189f10adb970451c
qq449245884 <449245884@qq.com>
2020-06-24 18:30:17 +0800

    88

--- 正则表达式/regex-playlist-lesson-10/regex-playlist-lesson-10/validation.js
+++ 正则表达式/regex-playlist-lesson-10/regex-playlist-lesson-10/validation.js
@@ -1,1 +1,25 @@
 // validation script here
+
+const patterns = {
+  // telephone: /[0-9]/
+  telephone: /^\d{11}$/
+}
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    // console.log(e.target.attributes.name.value)
+    // console.log(e.target, patterns['telephone'])
+    validate(e.target, patterns[e.target.attributes.name.value])
+  })
+})
+
+function validate(field, regex) {
+  // console.log(regex.test(field.value))
+  if (regex.test(field.value)) {
+    field.className = 'valid'
+  } else {
+    field.className = 'invalid'
+  }
+}
