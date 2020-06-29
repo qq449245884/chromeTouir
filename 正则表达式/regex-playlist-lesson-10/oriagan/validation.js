c539d3c9acd7ad55fd7230cf3abe377c081c9afc
qq449245884 <449245884@qq.com>
2020-06-30 00:51:01 +0800

    55

--- 正则表达式/regex-playlist-lesson-10/oriagan/validation.js
+++ 正则表达式/regex-playlist-lesson-10/oriagan/validation.js
@@ -6,6 +6,7 @@ const patterns = {
   // password: /^[a-z\d]$/,
   password: /^[\w@-]{8,20}$/,
   slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{3,8})(\.[a-z]{2,8})?$/,
 };
 
 const inputs = document.querySelectorAll('input')
