c7511838765c79e69b7a0e1eb22e28a266db907c
qq449245884 <449245884@qq.com>
2020-06-29 19:16:15 +0800

    88

--- 正则表达式/regex-playlist-lesson-10/regex-playlist-lesson-10/validation.js
+++ 正则表达式/regex-playlist-lesson-10/regex-playlist-lesson-10/validation.js
@@ -4,7 +4,8 @@ const patterns = {
   telephone: /^1\d{10}$/,
   username: /^[a-z]{5,12}$/i,
   password: /^[\w@-]{8,20}$/,
-  slug: /^[a-z\d-]{8,20}$/
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 const inputs = document.querySelectorAll('input')
